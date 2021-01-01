12cfcb1620d6418571db333f1c557050c2688419
vipul-anky <vipul.tanur@gmail.com>
2021-01-02 01:08:43 +0530

    hackerrank questions

--- src/main/java/ds_facebook/MinimumLengthSubString.java
+++ src/main/java/ds_facebook/MinimumLengthSubString.java
@@ -1,5 +1,91 @@
 package ds_facebook;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class MinimumLengthSubString {
 
+    static int minLengthSubstring(String s, String t) {
+        // Write your code here
+        Map<String, Long> ocrMap = Stream.of(t.split("")).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        String[] s1 = s.split("");
+        Map<String, Integer> windowMap = new HashMap<>();
+        int start =0;
+        int length = -1;
+        for (int i=0;i<s1.length;i++) {
+            String a = s1[i];
+            if (windowMap.containsKey(a)) {
+                int value = windowMap.get(a);
+                windowMap.put(a, ++value);
+            }
+            else {
+                windowMap.put(a, 1);
+            }
+
+            Set<String> sets= windowMap.keySet().stream().filter(ocrMap::containsKey).collect(Collectors.toSet());
+            //System.out.println(sets);
+            if (sets.equals(ocrMap.keySet())){
+                //now it is time to contract the window
+                //System.out.println("Length: "+length +", subString: "+s.substring(start, i+1));
+                boolean isWindowValid = validateMapWindow(ocrMap, windowMap);
+                while (isWindowValid) {
+                    length = (i+1) - start;
+                    String b = s1[start];
+                    int value = windowMap.get(b);
+                    windowMap.put(b, --value);
+                    isWindowValid = validateMapWindow(ocrMap, windowMap);
+                    if (!isWindowValid) {
+                        start += 1;
+                        break;
+                    }
+                    else {
+                        start = start +1;
+                        length = (i+1)-start;
+                  //      System.out.println("Length: "+length +", subString: "+s.substring(start, i+1));
+                    }
+
+                }
+            }
+        }
+        return length;
+    }
+
+    private static boolean validateMapWindow(Map<String,Long> ocrMap, Map<String,Integer> windowMap) {
+
+        for (String key : ocrMap.keySet()) {
+            Long minOcr = ocrMap.get(key);
+            int ocrInWindow = windowMap.get(key);
+            if (ocrInWindow < minOcr) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "dcbefebce";
+        String t = "fd";
+
+        String s1 = "ADOBECODEBANC";
+        String t1 = "ABC";
+        System.out.println(minLengthSubstring(s, t));
+        System.out.println(minLengthSubstring(s1, t1));
+
+        String s2 = "ABAACBAB";
+        String t2 = "ABC";
+        System.out.println(minLengthSubstring(s2, t2));
+
+        String s3 = "bfbeadbcbcbfeaaeefcddcccbbbfaaafdbebedddf";
+        String t3 = "cbccfafebccdccebdd";
+        System.out.println(minLengthSubstring(s3, t3));
+
+        String s4 = "cabwefgewcwaefgcf";
+        String t4 = "cae";
+        System.out.println(minLengthSubstring(s4, t4));
+    }
 }
--- src/main/java/ds_general/SortingAlgorithm.java
+++ src/main/java/ds_general/SortingAlgorithm.java
@@ -19,23 +19,38 @@ public class SortingAlgorithm {
         display(array);
     }
 
+    /**
+     * Quick sort
+     *
+     * @param arr   arr
+     * @param l     left
+     * @param h     right
+     * @return      pivot
+     */
     private static int partition(int[] arr, int l, int h) {
 
-        int pivot = arr[0];
+        int pivot = arr[l];
 
-        int i = l, j = h;
+        int i = l+1, j = h-1;
 
         while (i<j) {
             do {
+                if (arr[i] <= pivot)
                 i++;
-            }while (i < j && arr[i] < pivot);
+            }while (i <= arr.length-1 && arr[i] <= pivot);
 
+            if(i >=arr.length) {
+                i = arr.length-1;
+            }
             do {
-                j--;
-            }while (i < j && arr[j] > pivot);
+                if (arr[j] > pivot){
+                    j--;
+                }
+            }while (arr[j] > pivot);
             if (i<j)
             swap(arr, i, j);
         }
+        if (l!=j && arr[l] > arr[j])
         swap(arr, l, j);
         return j;
     }
@@ -45,8 +60,8 @@ public class SortingAlgorithm {
 
         if (low < high) {
             int index  = partition(array, low, high);
-            quickSort(array, index+1, high);
             quickSort(array, low, index);
+            quickSort(array, index+1, high);
         }
     }
 
@@ -58,16 +73,31 @@ public class SortingAlgorithm {
 
 
     private static void display(int[] array) {
-        for (int i=0;i<array.length;i++) {
-            System.out.print(array[i]+" ");
+        System.out.println();
+        for (int value : array) {
+            System.out.print(value + " ");
         }
+        System.out.println();
     }
 
     public static void main(String[] args) {
         int[] array_1 = {7,8,10,5,20,90,32,88};
         int[] array_2 = {7,76,32,13,17,20,10,11};
+        int[] array_3 = {7,8,10,5,20};
+        int[] array_4 = {10, 80, 30, 90, 40, 50, 70};
+        int[] array_5 = {10, 7, 8, 9, 1, 5};
+        int[] array_6 = {};
         selectionSort(array_1);
+        quickSort(array_3, 0, array_3.length);
+        display(array_3);
         quickSort(array_2, 0, array_2.length);
         display(array_2);
+        quickSort(array_4, 0, array_4.length);
+        display(array_4);
+        quickSort(array_5, 0, array_5.length);
+        display(array_5);
+        quickSort(array_6,0, array_6.length);
+        display(array_6);
+
     }
 }
