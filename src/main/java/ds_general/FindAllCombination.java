a3fe424b56e74e66af513568e5f3a62d094929f2
vipul-anky <vipul.tanur@gmail.com>
2021-01-07 00:51:18 +0530

    hackerrank questions

--- src/main/java/ds_general/FindAllCombination.java
+++ src/main/java/ds_general/FindAllCombination.java
@@ -5,56 +5,54 @@ import java.util.List;
 
 public class FindAllCombination {
 
-    static void findAllChoices(int[] candidates, int target, int start, List<List<Integer>> answer, List<Integer> temp) {
+    static List<List<Integer>> combinationSum = new ArrayList<>();
 
+    static List<Integer> integerList = new ArrayList<>();
+
+    public static List<Integer> getAllChoices(int index, int[] candidates, int target) {
+
+        if (target < 0) {
+            return new ArrayList<>();
+        }
         if (target == 0) {
-            answer.add(new ArrayList<>(temp));
-            return;
+            return new ArrayList<>();
+        }
+        if (index == candidates.length-1) {
+            List<Integer> p = new ArrayList<>();
+            p.add(candidates[index]);
+            return p;
         }
 
-        for (int i = start; i < candidates.length; i++) {
-            target -= candidates[i];
-            // if target minus the candidate is less than zero, then there's no need to try the rest candidates
-            // because the rest candidats are bigger than the current candidate
-            if (target < 0) {
-                break;
-            }
+        int number = candidates[index];
+        List<Integer> choices = getAllChoices(index+1, candidates, target);
+        List<Integer> singleNumber  = getAllChoices(index, candidates, target - candidates[index]);
+
+        System.out.println(singleNumber);
+        return integerList;
+    }
+
+    public static void getAllChoices(int index, int[] candidates, int target, List<Integer> temp, List<List<Integer>> result) {
+
+        if (target < 0) return;
+        if (target == 0)  {
+            result.add(new ArrayList<>(temp));
+        }
+
+        for (int i=index;i<candidates.length;i++) {
             temp.add(candidates[i]);
-            findAllChoices(candidates, target, i, answer, temp);
-            temp.remove(temp.size() - 1);
-            target += candidates[i];
+            getAllChoices(i, candidates, target-candidates[i], temp, result);
+            temp.remove(temp.size() -1);
         }
     }
 
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
 
-    static List<List<Integer>> findCombinations(int[] n, int target) {
+        System.out.println(getAllChoices(0, candidates, target));
 
-        List<List<Integer>> combinationsList = new ArrayList<>();
-        findAllChoices(n, target, 0, combinationsList, new ArrayList<>());
-
-        return combinationsList;
-    }
-
-
-    static List<List<Integer>> allChoices = new ArrayList<>();
-
-    static void findAllChoices(int[] n, int target, List<Integer> combination) {
-        if (target == 0) {
-            allChoices.add(combination);
-            return;
-        }
-        for (int value : n) {
-            target -= value;
-
-            if (target < 0) {
-                break;
-            }
-
-            combination.add(value);
-            findAllChoices(n, target, combination);
-            combination.remove(combination.size() - 1);
-            target += value;
-        }
+        List<List<Integer>> result = new ArrayList<>();
+        getAllChoices(0, candidates, target, new ArrayList<>(), result);
+        System.out.println(result);
+        return combinationSum;
     }
 
     public static void main(String[] args) {
@@ -62,10 +60,9 @@ public class FindAllCombination {
 
         //System.out.println(findCombinations(array, 7));
 
-        int[] array_1 = {2,3,6,7};
-        //System.out.println(findCombinations(array_1, 7));
+        int[] array_1 = {2,3};
+        System.out.println(combinationSum(array_1, 5));
 
-        findAllChoices(array, 7, new ArrayList<>());
-        System.out.println(allChoices);
+
     }
 }
